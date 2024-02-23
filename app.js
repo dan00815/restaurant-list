68b779e2686915858911daf59dd3c0c61eec47bf
Dan Wu <Wu@gmail.com>
2024-02-23 10:09:15 +0800

    set express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { engine } = require("express-handlebars");
 
 //middleware
 app.use(express.static("public"));
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
 app.get("/", (req, res) => {
   res.redirect("/restaurant");
 });
 app.get("/restaurant", (req, res) => {
-  res.send("首頁");
+  res.render("index");
 });
 
 app.listen(port, () => console.log("server is listening on port 3000"));
