d70bce2f6de804505d37a99cd10b800520099df7
Dan Wu <Wu@gmail.com>
2024-02-26 09:17:53 +0800

    add search function & search-failed page

--- app.js
+++ app.js
@@ -15,17 +15,39 @@ app.get("/", (req, res) => {
 });
 
 app.get("/restaurants", (req, res) => {
-  res.render("index", { restaurant });
+  const keyword = req.query.keyword?.trim().toLowerCase(); //string
+
+  const searchResult = keyword
+    ? restaurant.filter((item) =>
+        Object.keys(item).some((keyproperty) => {
+          if (
+            keyproperty === "name" ||
+            keyproperty === "category" ||
+            keyproperty === "location" ||
+            keyproperty === "description"
+          ) {
+            // console.log(item[keyproperty]);
+            return item[keyproperty].toLowerCase().includes(keyword);
+          }
+          return false;
+        })
+      )
+    : restaurant;
+  //關鍵字搜尋，希望用戶可以從除了店名以外的地方搜尋到，但又希望就實際體驗來看，只能從點進去的頁面中所見的資訊(名字、類別、地址、詳細介紹)這些文字中搜尋
+
+  //找不到搜尋結果
+  if (!searchResult.length) {
+    res.render("search-failed", { keyword });
+    return;
+  }
+
+  res.render("index", { restaurant: searchResult, keyword });
 });
 
 app.get("/restaurants/:id", (req, res) => {
   const id = req.params.id; //string
   const selectedRestaurant = restaurant.find((item) => item.id === +id);
   res.render("selectedPage", { selectedRestaurant });
-});
-
-app.get("/sss", (req, res) => {
-  res.send(restaurant);
 });
 
 app.listen(port, () => console.log("server is listening on port 3000"));
